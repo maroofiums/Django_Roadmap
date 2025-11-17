490bf75ab21ec34f979d3f35b1468f0c40d09d5d
maroof2424 <maroof96965@gmail.com>
2025-11-17 06:55:49 +0500

    Week8 Day7

--- Week08_CBVs_Middleware/blog_project/blog/forms.py
+++ Week08_CBVs_Middleware/blog_project/blog/forms.py
@@ -13,19 +13,46 @@ class PostForm(forms.ModelForm):
         }
 
 
+
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={
-        "class": "form-control",
-        "placeholder": "Email"
-    }))
-    
+
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter password'
+            }
+        )
+    )
+
+    password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm password'
+            }
+        )
+    )
+
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'email']
+
         widgets = {
-            "username": forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}),
-            "password1": forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}),
-            "password2": forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Confirm Password"}),
+            'username': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Choose a username'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter your email'
+                }
+            ),
         }
 
 class LoginForm(AuthenticationForm):
