6e5f475f09a72ad2f378112cdd46368c60850cc9
maroof2424 <maroof96965@gmail.com>
2025-11-20 12:23:42 +0500

    Week9 Day3

--- Week09_CrispyForms_AdvancedForms/FormTutorial/book_form/forms.py
+++ Week09_CrispyForms_AdvancedForms/FormTutorial/book_form/forms.py
@@ -2,8 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Field, Submit, Div
+from crispy_forms.layout import Layout, Row, Column, Div, Submit, Field
 
+from .models import Book
+
+
+# -------------------------
+# SIGNUP FORM
+# -------------------------
 class SignupForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
@@ -16,15 +22,16 @@ class SignupForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
+
         self.helper.layout = Layout(
             Div(
                 Field("username", placeholder="Enter username"),
                 Field("email", placeholder="Enter email"),
                 Field("password", placeholder="Enter password"),
                 Field("confirm_password", placeholder="Confirm password"),
-                css_class="form-group",
+                css_class="card p-4 shadow"
             ),
-            Submit("submit", "Sign Up", css_class="btn btn-primary mt-3"),
+            Submit("submit", "Sign Up", css_class="btn btn-primary w-100 mt-3"),
         )
 
     def clean(self):
@@ -41,13 +48,42 @@ class SignupForm(forms.ModelForm):
         return user
 
 
+# -------------------------
+# LOGIN FORM
+# -------------------------
 class LoginForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
         super().__init__(request, *args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "post"
+
         self.helper.layout = Layout(
             Field("username", placeholder="Username"),
             Field("password", placeholder="Password"),
-            Submit("submit", "Login", css_class="btn btn-success mt-3"),
+            Submit("submit", "Login", css_class="btn btn-success w-100 mt-3"),
+        )
+
+
+# -------------------------
+# BOOK FORM
+# -------------------------
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ["title", "author"]  # exclude published_date
+        # published_date will be auto set
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+
+        self.helper.layout = Layout(
+            Div(
+                Row(Column("title", css_class="form-floating mb-3")),
+                Row(Column("author", css_class="form-floating mb-3")),
+                css_class="card p-4 shadow"
+            ),
+            Submit("submit", "Save Book", css_class="btn btn-primary w-100 mt-3"),
         )
--- Week09_CrispyForms_AdvancedForms/FormTutorial/book_form/views.py
+++ Week09_CrispyForms_AdvancedForms/FormTutorial/book_form/views.py
@@ -1,40 +1,48 @@
-from django.views.generic import (
-    ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
-)
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
-from django.contrib.auth import login
 from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
+
 from .models import Book
-from .forms import LoginForm, SignupForm
+from .forms import BookForm, LoginForm, SignupForm
+
+
+# -------------------------
+# BOOK VIEWS
+# -------------------------
 
 class BookListView(ListView):
     model = Book
     template_name = "FormApp/book_list.html"
     context_object_name = "books"
 
+
 class BookDetailView(DetailView):
     model = Book
     template_name = "FormApp/book_detail.html"
 
+
 class BookCreateView(LoginRequiredMixin, CreateView):
     model = Book
-    fields = ["title", "author", "published_date"]
     template_name = "FormApp/book_form.html"
+    form_class = BookForm
     success_url = reverse_lazy("book_list")
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
+
 class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Book
-    fields = ["title", "author", "published_date"]
     template_name = "FormApp/book_form.html"
+    form_class = BookForm
     success_url = reverse_lazy("book_list")
 
     def test_func(self):
         return self.get_object().owner == self.request.user
+
 
 class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Book
@@ -43,6 +51,11 @@ class BookDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         return self.get_object().owner == self.request.user
+
+
+# -------------------------
+# AUTH VIEWS
+# -------------------------
 
 class SignUpView(FormView):
     template_name = "FormApp/signup.html"
@@ -54,9 +67,11 @@ class SignUpView(FormView):
         login(self.request, user)
         return super().form_valid(form)
 
+
 class LoginView(DjangoLoginView):
     template_name = "FormApp/login.html"
     authentication_form = LoginForm
+
 
 class LogoutView(DjangoLogoutView):
     next_page = reverse_lazy("login")
