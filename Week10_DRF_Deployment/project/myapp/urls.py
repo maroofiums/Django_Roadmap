440eda2cd540ffbfe67445d81f2d0a8e1ad72152
maroof2424 <maroof96965@gmail.com>
2025-11-28 20:41:50 +0500

    Week10 Day3

--- Week10_DRF_Deployment/project/myapp/urls.py
+++ Week10_DRF_Deployment/project/myapp/urls.py
@@ -1,16 +1,27 @@
 from .views import (
-    PostListAPIView,
-    PostDetailAPIView,
-    PostCreateAPI,
-    PostUpdateAPI,
-    PostDeleteAPI
-)
-from django.urls import path
+#     PostListAPIView,
+#     PostDetailAPIView,
+#     PostCreateAPI,
+#     PostUpdateAPI,
+#     PostDeleteAPI
+    PostViewSet
+ )
+from django.urls import path,include
+
+# urlpatterns = [
+#     path('', PostListAPIView.as_view(),name = "list_post"),
+#     path('detail/<int:pk>', PostDetailAPIView.as_view(),name = "post_detail"),
+#     path('new/', PostCreateAPI.as_view(),name = "create_post"),
+#     path('update/<int:pk>', PostUpdateAPI.as_view(),name = "update_post"),
+#     path('delete/<int:pk>', PostDeleteAPI.as_view(),name = "delete_post"),
+# ]
+
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register("posts", PostViewSet)
 
 urlpatterns = [
-    path('', PostListAPIView.as_view(),name = "list_post"),
-    path('detail/<int:pk>', PostDetailAPIView.as_view(),name = "post_detail"),
-    path('new/', PostCreateAPI.as_view(),name = "create_post"),
-    path('update/<int:pk>', PostUpdateAPI.as_view(),name = "update_post"),
-    path('delete/<int:pk>', PostDeleteAPI.as_view(),name = "delete_post"),
+    path("", include(router.urls)),
 ]
--- Week10_DRF_Deployment/project/myapp/views.py
+++ Week10_DRF_Deployment/project/myapp/views.py
@@ -1,46 +1,52 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostListAPIView(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+# class PostListAPIView(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
 
 
-class PostDetailAPIView(APIView):
-    def get(self,request,pk):
-        try:
-            post = Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            return Response({"error":"Post Not Found"},status=status.HTTP_404_NOT_FOUND)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+# class PostDetailAPIView(APIView):
+#     def get(self,request,pk):
+#         try:
+#             post = Post.objects.get(pk=pk)
+#         except Post.DoesNotExist:
+#             return Response({"error":"Post Not Found"},status=status.HTTP_404_NOT_FOUND)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
 
-class PostCreateAPI(APIView):
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+# class PostCreateAPI(APIView):
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=201)
+#         return Response(serializer.errors, status=400)
 
-class PostUpdateAPI(APIView):
-    def put(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=400)
+# class PostUpdateAPI(APIView):
+#     def put(self, request, pk):
+#         post = Post.objects.get(pk=pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=400)
 
-class PostDeleteAPI(APIView):
-    def delete(self, request, pk):
-        post = Post.objects.get(pk=pk)
-        post.delete()
-        return Response({"message": "deleted"})
+# class PostDeleteAPI(APIView):
+#     def delete(self, request, pk):
+#         post = Post.objects.get(pk=pk)
+#         post.delete()
+#         return Response({"message": "deleted"})
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by("-created_at")
+    serializer_class = PostSerializer
+
