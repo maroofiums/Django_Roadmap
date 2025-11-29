4988cc61058316a7c4dc43ceccb4a0dadac51d11
maroof2424 <maroof96965@gmail.com>
2025-11-29 08:00:13 +0500

    Week10 Day4

--- Week10_DRF_Deployment/project/myapp/urls.py
+++ Week10_DRF_Deployment/project/myapp/urls.py
@@ -1,21 +1,5 @@
-from .views import (
-#     PostListAPIView,
-#     PostDetailAPIView,
-#     PostCreateAPI,
-#     PostUpdateAPI,
-#     PostDeleteAPI
-    PostViewSet
- )
+from .views import PostViewSet
 from django.urls import path,include
-
-# urlpatterns = [
-#     path('', PostListAPIView.as_view(),name = "list_post"),
-#     path('detail/<int:pk>', PostDetailAPIView.as_view(),name = "post_detail"),
-#     path('new/', PostCreateAPI.as_view(),name = "create_post"),
-#     path('update/<int:pk>', PostUpdateAPI.as_view(),name = "update_post"),
-#     path('delete/<int:pk>', PostDeleteAPI.as_view(),name = "delete_post"),
-# ]
-
 from rest_framework.routers import DefaultRouter
 
 
