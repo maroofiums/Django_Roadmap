f726b7d0d4a179485cb0f3f1278e7f93d7f8b49b
maroof2424 <maroof96965@gmail.com>
2025-11-29 08:31:59 +0500

    Week10 Day5

--- Week10_DRF_Deployment/project/myapp/views.py
+++ Week10_DRF_Deployment/project/myapp/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by("-created_at")
     serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.method)
