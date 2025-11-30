0898656cd31a73e91953f6028a9b79c823e4c76a
maroof2424 <maroof96965@gmail.com>
2025-11-30 20:20:08 +0500

    Week10 Day6

--- Week10_DRF_Deployment/project/myapp/urls.py
+++ Week10_DRF_Deployment/project/myapp/urls.py
@@ -2,7 +2,6 @@ from .views import PostViewSet
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 
-
 router = DefaultRouter()
 router.register("posts", PostViewSet)
 
--- Week10_DRF_Deployment/project/myapp/utils.py
+++ Week10_DRF_Deployment/project/myapp/utils.py
@@ -1,8 +1,6 @@
 from faker import Faker
 from .models import Post
-
 fake = Faker()
-
 def generate_fake_posts(n=10):
     for _ in range(n):
         Post.objects.create(
