6feac4119a29f8c4c9e042b43173215d6051099c
maroof2424 <maroof96965@gmail.com>
2025-12-03 20:40:06 +0500

    Week11 Day2

--- Week11_Signals_Caching/myproject/myapp/models.py
+++ Week11_Signals_Caching/myproject/myapp/models.py
@@ -8,3 +8,11 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- Week11_Signals_Caching/myproject/myapp/signals.py
+++ Week11_Signals_Caching/myproject/myapp/signals.py
@@ -1,9 +1,26 @@
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile
+from django.contrib.auth.models import User
 
-@receiver(post_save,sender=User)
-def create_profile(sender,instance,created,**kwargs):
+from .models import Profile, Post
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+        print("Profile Auto Created!")
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    print("Profile Auto Saved!")
+
+
+@receiver(post_save, sender=Post)
+def post_saved(sender, instance, created, **kwargs):
+    if created:
+        print("New Post Created:", instance.title)
+    else:
+        print("Post Updated:", instance.title)
