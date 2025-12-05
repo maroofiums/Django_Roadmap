4693a34aa51cb4add539dc6b0ddb3ba3ad436534
maroof2424 <maroof96965@gmail.com>
2025-12-05 13:34:45 +0500

    Week11 Day5

--- Week11_Signals_Caching/myproject/myapp/urls.py
+++ Week11_Signals_Caching/myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView,get_popular_posts
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+    path("popular/",get_popular_posts)
 ]
--- Week11_Signals_Caching/myproject/myapp/views.py
+++ Week11_Signals_Caching/myproject/myapp/views.py
@@ -9,3 +9,13 @@ class HomeView(View):
     def get(self, request):
         posts = Post.objects.all()
         return render(request, 'home.html', {'recent_posts': posts})
+from django.core.cache import cache
+
+def get_popular_posts(request):
+    posts = cache.get('popular_posts')
+
+    if not posts:
+        posts = Post.objects.order_by('-created_at')[:5]
+        cache.set('popular_posts', posts, timeout=30)
+
+    return render(request, "popular.html", {"posts": posts})
