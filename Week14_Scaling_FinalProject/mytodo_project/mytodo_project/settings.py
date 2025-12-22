2fcb2b1a73aa712057f4cd4a25efc9fbcc018ba1
maroof2424 <maroof96965@gmail.com>
2025-12-22 21:15:24 +0500

    Week14 Day1

--- Week14_Scaling_FinalProject/mytodo_project/mytodo_project/settings.py
+++ Week14_Scaling_FinalProject/mytodo_project/mytodo_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "mytodo",
+    
 ]
 
 MIDDLEWARE = [
@@ -73,9 +75,13 @@ WSGI_APPLICATION = 'mytodo_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydb",
+        "USER": "postgres",
+        "PASSWORD": "whynot",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
