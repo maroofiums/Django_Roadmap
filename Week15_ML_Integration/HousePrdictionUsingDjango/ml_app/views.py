87c131fe69494464bfc3be7f769dbce12388086c
maroof2424 <maroof96965@gmail.com>
2025-12-31 12:31:24 +0500

    Week15 Day3

--- Week15_ML_Integration/HousePrdictionUsingDjango/ml_app/views.py
+++ Week15_ML_Integration/HousePrdictionUsingDjango/ml_app/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+# ml_app/views.py
 
-from HousePrdictionUsingDjango.loaders import load_model
+from django.http import JsonResponse
+from HousePrdictionUsingDjango.loaders import model
 
-def predict_price(area, rooms):
-    model = load_model()
-    prediction = model.predict([[area, rooms]])
-    return prediction[0]
+def predict_price(request):
+    area = int(request.GET.get("area", 1000))
+    rooms = int(request.GET.get("rooms", 2))
 
+    prediction = model.predict([[area, rooms]])[0]
+
+    return JsonResponse({
+        "area": area,
+        "rooms": rooms,
+        "predicted_price": round(prediction, 2)
+    })
